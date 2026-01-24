5e9baca550d83d151924d223b819f11b6d4debc6
JManzanilla <ing.jesus.manzanilla.gomez@gmail.com>
2026-01-23 19:15:25 -0600

    Resolver conflicto de merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
 import "@/index.css";
 import AppRouter from "./AppRouter";
-=======
-import "./index.css";
-import App from "./components/App";
->>>>>>> main
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppRouter />
-  </StrictMode>
+  </StrictMode>,
 );
